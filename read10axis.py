187b8064498df4bc94297c68ccf64b8605677c4b
Michael Lee <tylee@ieee.org>
2018-03-19 22:42:09 +0800

    Create 6axis, 9axis and 10axis version

--- read10axis.py
+++ read10axis.py
@@ -7,5 +7,5 @@ MPU9250 = imu.MPU9250()
 MPU9250.configMPU9250(imu.GFS_250, imu.AFS_8G)
 MPU9250.configAK8963(imu.AK8963_MODE_C100HZ, imu.AK8963_BIT_16)
 while True:
-    print(f"A={MPU9250.readAccel()}, G={MPU9250.readGyro()}, M={MPU9250.readMagnet()}")
+    print(f"A={MPU9250.readAccel()}, G={MPU9250.readGyro()}, M={MPU9250.readMagnet()}, T={MPU9250.readTemperature()}")
     time.sleep(0.1)
